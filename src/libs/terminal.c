f4db28ab18782fda35b6350d0d38d88519c78bdb
ghazan54 <cegou520@mail.ru>
2023-02-13 19:29:15 +0300

    [fix] Fixed all terminal functions

--- src/libs/terminal.c
+++ src/libs/terminal.c
@@ -1,4 +1,8 @@
+
+#include <errno.h>
 #include <fcntl.h>
+#include <linux/fs.h>
+#include <net/if.h>
 #include <sc/terminal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,62 +12,69 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int mt_clrscr(void) {
+int mt_clrscr(void)
+{
     int term;
     if ((term = open(TERMINAL, O_WRONLY)) == -1) {
         return EXIT_FAILURE;
     }
-    int c = write(term, TERM_CLEAR_SCREEN, sizeof(TERM_CLEAR_SCREEN));
-    close(term);
-    return c == sizeof(TERM_CLEAR_SCREEN) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
-
-int mt_gotoXY(int x, int y) {
-    if (x < 0 || y < 0) {
-        return EXIT_FAILURE;
-    }
-    int term;
-    if ((term = open(TERMINAL, O_WRONLY)) == -1) {
-        return EXIT_FAILURE;
-    }
-    char s[BUFSIZ] = {0};
-    sprintf(s, "\e[%d;%df", x, y);
-    int c = write(term, s, BUFSIZ);
+    int c = write(term, "\e[H\e[J", strlen("\e[H\e[J"));
     close(term);
     return c != -1 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-int mt_getscreensize(int* rows, int* cols) {
+int mt_getscreensize(int* rows, int* columns)
+{
     struct winsize ws;
-    if (!rows || !cols || ioctl(1, TIOCGWINSZ, &ws)) {
+    if (!rows || !columns || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)) {
         return EXIT_FAILURE;
     } else {
         *rows = ws.ws_row;
-        *cols = ws.ws_col;
+        *columns = ws.ws_col;
         return EXIT_SUCCESS;
     }
 }
 
-int mt_setfgcolor(enum colors color) {
+int mt_gotoXY(int x, int y)
+{
+    int row, col;
+    mt_getscreensize(&row, &col);
+    if (((x > col) || (x < 0)) || ((y > row) || (y < 0))) {
+        return EXIT_FAILURE;
+    }
+    char s[BUFSIZ];
+    sprintf(s, "\e[%d;%dH", x, y);
     int term;
     if ((term = open(TERMINAL, O_WRONLY)) == -1) {
         return EXIT_FAILURE;
     }
-    char s[BUFSIZ] = {0};
-    sprintf(s, "\e[3%dm", color);
-    int c = write(term, s, BUFSIZ);
+    int c = write(term, s, strlen(s));
     close(term);
     return c != -1 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-int mt_setbgcolor(enum colors color) {
+int mt_setfgcolor(enum colors color)
+{
+    char s[BUFSIZ];
+    sprintf(s, "\e[0;3%dm", color);
     int term;
     if ((term = open(TERMINAL, O_WRONLY)) == -1) {
         return EXIT_FAILURE;
     }
-    char s[BUFSIZ] = {0};
+    int c = write(term, s, strlen(s));
+    close(term);
+    return c != -1 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+int mt_setbgcolor(enum colors color)
+{
+    char s[BUFSIZ];
     sprintf(s, "\e[4%dm", color);
-    int c = write(term, s, BUFSIZ);
+    int term;
+    if ((term = open(TERMINAL, O_WRONLY)) == -1) {
+        return EXIT_FAILURE;
+    }
+    int c = write(term, s, strlen(s));
     close(term);
     return c != -1 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
