9641ddd8677b39a9943b9190e464662b542ab766
ghazan54 <cegou520@mail.ru>
2023-03-18 14:52:25 +0300

    [fix] Fixed reading

--- src/libs/CU.c
+++ src/libs/CU.c
@@ -7,25 +7,30 @@
 int operations = 0;
 int accumulator = 0;
 
-int CU(int operation) {
+int CU(int operation)
+{
     int command, operand;
-    if (sc_commandDecode(operation, &command, &operand)) return EXIT_FAILURE;
+    if (sc_commandDecode(operation, &command, &operand))
+        return EXIT_FAILURE;
     switch (command) {
-        case 10:
-            return cu_read(operand);
-        case 11:
-            return cu_write(operand);
-        default:
-            break;
+    case 10:
+        return cu_read(operand);
+    case 11:
+        return cu_write(operand);
+    default:
+        break;
     }
     return EXIT_FAILURE;
 }
 
-int cu_read(int operand) {
-    if (rk_mytermregime(0, 0, 4, 1, 0)) return EXIT_FAILURE;
-    char bf[5] = {0};
+int cu_read(int operand)
+{
+    if (rk_mytermregime(0, 0, 4, 1, 0))
+        return EXIT_FAILURE;
+    char bf[5] = { 0 };
     I_printInputField(1, "Read: ");
-    if (read(STDIN_FILENO, bf, 4) == -1) return EXIT_FAILURE;
+    if (read(STDIN_FILENO, bf, 4) == -1)
+        return EXIT_FAILURE;
     long long c = xtoll(bf);
     if (c > INT32_MAX || c < INT32_MIN) {
         sc_regSet(err_out_of_range, 1);
@@ -34,10 +39,12 @@ int cu_read(int operand) {
     if (!c && bf[0] != '0') {
         return EXIT_FAILURE;
     }
+    I_printInputField(0, NULL);
     return sc_memorySet(operand, c);
 }
 
-int cu_write(int operand) {
+int cu_write(int operand)
+{
     int c, ret = sc_memoryGet(operand, &c);
     return I_printOutputField("%04X", c) && ret;
 }
--- src/libs/bigchar.c
+++ src/libs/bigchar.c
@@ -6,103 +6,40 @@
 #include <string.h>
 #include <unistd.h>
 
-char big_char_plus[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0,
-                        0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-char big_char_minus[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-char big_char_0[] = {0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-                     0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
-                     0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0};
-
-char big_char_1[] = {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0,
-                     1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-                     0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0};
-
-char big_char_2[] = {0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-                     1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
-                     0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0};
-
-char big_char_3[] = {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-                     1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-                     0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0};
-
-char big_char_4[] = {0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-                     1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-                     0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0};
-
-char big_char_5[] = {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                     0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-                     0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0};
-
-char big_char_6[] = {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                     0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,
-                     0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0};
-
-char big_char_7[] = {0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-                     1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-                     0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0};
-
-char big_char_8[] = {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-                     1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,
-                     0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0};
-
-char big_char_9[] = {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-                     1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-                     0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0};
-
-char big_char_A[] = {0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-                     1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,
-                     0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0};
-
-char big_char_B[] = {0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-                     1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,
-                     0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0};
-
-char big_char_C[] = {0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                     0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-                     0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0};
-
-char big_char_D[] = {0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
-                     1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,
-                     0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0};
-
-char big_char_E[] = {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                     0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-                     0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0};
-
-char big_char_F[] = {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                     0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-                     0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0};
-
 int bc_printA(const char* str) { return str && printf("\e(0%s\e(B", str) ? EXIT_SUCCESS : EXIT_FAILURE; }
 
-int bc_box(int x1, int y1, const int x2, const int y2) {
-    if (x1 > x2 || y1 > y2 || x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) return EXIT_FAILURE;
+int bc_box(int x1, int y1, const int x2, const int y2)
+{
+    if (x1 > x2 || y1 > y2 || x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0)
+        return EXIT_FAILURE;
     bool first = true;
     for (; x1 <= x2; ++x1, first = false) {
-        if (mt_gotoXY(x1, y1)) return EXIT_FAILURE;
+        if (mt_gotoXY(x1, y1))
+            return EXIT_FAILURE;
         if (first) {
             for (int _y1 = y1; _y1 <= y2; ++_y1) {
                 if (_y1 == y1) {
-                    if (bc_printA(ACS_ULCORNER)) return EXIT_FAILURE;
+                    if (bc_printA(ACS_ULCORNER))
+                        return EXIT_FAILURE;
                 } else if (_y1 == y2) {
-                    if (bc_printA(ACS_URCORNER)) return EXIT_FAILURE;
+                    if (bc_printA(ACS_URCORNER))
+                        return EXIT_FAILURE;
                 } else {
-                    if (bc_printA(ACS_HLINE)) return EXIT_FAILURE;
+                    if (bc_printA(ACS_HLINE))
+                        return EXIT_FAILURE;
                 }
             }
         } else if (x1 == x2) {
             for (int _y1 = y1; _y1 <= y2; ++_y1) {
                 if (_y1 == y1) {
-                    if (bc_printA(ACS_LLCORNER)) return EXIT_FAILURE;
+                    if (bc_printA(ACS_LLCORNER))
+                        return EXIT_FAILURE;
                 } else if (_y1 == y2) {
-                    if (bc_printA(ACS_LRCORNER)) return EXIT_FAILURE;
+                    if (bc_printA(ACS_LRCORNER))
+                        return EXIT_FAILURE;
                 } else {
-                    if (bc_printA(ACS_HLINE)) return EXIT_FAILURE;
+                    if (bc_printA(ACS_HLINE))
+                        return EXIT_FAILURE;
                 }
             }
         } else {
@@ -116,14 +53,20 @@ int bc_box(int x1, int y1, const int x2, const int y2) {
     return EXIT_SUCCESS;
 }
 
-int bc_printbigchar(int n[2], int x, int y, enum colors fg, enum colors bg) {
-    if (mt_setfgcolor(fg)) return EXIT_FAILURE;
+int bc_printbigchar(const int n1[2], int x, int y, enum colors fg, enum colors bg)
+{
+    int n[] = { 0, 0 };
+    memcpy(n, n1, sizeof(int) * 2);
+    if (mt_setfgcolor(fg))
+        return EXIT_FAILURE;
 
-    if (mt_setbgcolor(bg)) return EXIT_FAILURE;
+    if (mt_setbgcolor(bg))
+        return EXIT_FAILURE;
 
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 4; ++j) {
-            if (mt_gotoXY(x++, y)) return EXIT_FAILURE;
+            if (mt_gotoXY(x++, y))
+                return EXIT_FAILURE;
             for (int k = 0; k < 8; ++k) {
                 int bit = n[i] & 0x1;
                 n[i] >>= 1;
@@ -132,11 +75,13 @@ int bc_printbigchar(int n[2], int x, int y, enum colors fg, enum colors bg) {
             printf("\n");
         }
     }
-    if (mt_gotoXY(++x, y)) return EXIT_FAILURE;
+    if (mt_gotoXY(++x, y))
+        return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
 
-int bc_setbigcharpos(int* big, int x, int y, int value) {
+int bc_setbigcharpos(int* big, int x, int y, int value)
+{
     if (x < 0 || y < 0 || x > 7 || y > 7) {
         return EXIT_FAILURE;
     }
@@ -157,7 +102,8 @@ int bc_setbigcharpos(int* big, int x, int y, int value) {
     return EXIT_SUCCESS;
 }
 
-int bc_getbigcharpos(int* big, int x, int y, int* value) {
+int bc_getbigcharpos(int* big, int x, int y, int* value)
+{
     if (x < 0 || y < 0 || x > 7 || y > 7 || !value) {
         return EXIT_FAILURE;
     }
@@ -174,13 +120,17 @@ int bc_getbigcharpos(int* big, int x, int y, int* value) {
     return EXIT_SUCCESS;
 }
 
-int bc_bigcharwrite(int fd, int* big, int count) {
+int bc_bigcharwrite(int fd, int* big, int count)
+{
     return !big || write(fd, big, count * sizeof(int) * 2) == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
-int bc_bigcharread(int fd, int* big, int need_count, int* count) {
-    if (!big || !count) return EXIT_FAILURE;
+int bc_bigcharread(int fd, int* big, int need_count, int* count)
+{
+    if (!big || !count)
+        return EXIT_FAILURE;
     *count = read(fd, big, need_count * sizeof(int) * 2);
-    if (*count != -1) *count = *count / sizeof(int) / 2;
+    if (*count != -1)
+        *count = *count / sizeof(int) / 2;
     return !big || *count != need_count ? EXIT_FAILURE : EXIT_SUCCESS;
 }
