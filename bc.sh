3c09f69b302b4bbbbb8ffc27f779984f85a0f7b0
ghazan54 <cegou520@mail.ru>
2023-05-10 18:49:11 +0700

    [add] Added github CI

--- bc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-str="++\054\054--..00__``aaffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz{{||}c~~";
-
-for (( i=0; $i<${#str}; i=$(($i+1)) ))
-do
-echo -n -e "\e(0${str:$i:1}\e(B";
-done
-echo ""
